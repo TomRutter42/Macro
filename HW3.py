8a0b9d0c5f61902d6fa7533292291807e0f6ed45
Tom Rutter <67977402+TomRutter42@users.noreply.github.com>
2022-11-01 01:34:29 -0700

    testing

--- HW3.py
+++ HW3.py
@@ -54,6 +54,7 @@ def kstar_fn(R):
 
 # Define FOCs 
 
+@njit
 def foc_constrained(kprime, w_uncons,vprime_fn,phi,R,beta):
     """ 
     Fn to solve for unknown kprime
@@ -63,6 +64,7 @@ def foc_constrained(kprime, w_uncons,vprime_fn,phi,R,beta):
     output = uprime_fn(w_uncons - (1-phi)*kprime)*(1-phi) -beta*vprime_fn(f(kprime) -(R*phi*kprime))*(fprime(kprime)-R*phi)
     return output 
 
+@njit
 def foc_unconstrained(bprime, w_uncons,vprime_fn,phi,R,beta):
     """ 
     Fn to solve for unknown bprime
@@ -72,6 +74,7 @@ def foc_unconstrained(bprime, w_uncons,vprime_fn,phi,R,beta):
     output = uprime_fn(w_uncons- bprime - kstar) - beta*R*vprime_fn( f(kstar) +(R*bprime) )
     return output 
 
+@njit
 def threshold(wbar,vprime_fn,phi,R,beta):
     """
     Function to solve for unknown threshold wbar
@@ -90,13 +93,81 @@ w_grid = np.linspace(0.5, 5, 10)
 
 # Define a function that solves for the optimal kprime and bprime given w. 
 
-def solve_kprime_bprime(wgrid_arg, phi_arg, R_arg, beta_arg):
+@njit
+def solve_kprime_bprime(wgrid, phi, R, beta):
 
     # Form an initial guess for the value function 
 
-    vprime = np.ones(len(wgrid_arg))
+    vprime_old = np.ones(len(wgrid))
 
-    # Calculate 
+    # Set up empty arrays to store the optimal kprime and bprime
+
+    kprime = np.empty(len(wgrid))
+    bprime = np.empty(len(wgrid))
+
+    # Run the value function iteration algorithm. 
+
+    ## First, we specify the tolerance level for the algorithm.
+
+    tol = 1e-6
+
+    ## Next, we set up a while loop that will run until the distance between the old and new value functions is less than the tolerance level.
+
+    dist = 1
+
+    while dist > tol:
+
+        # Set up an empty array to store the new value function
+
+        vprime_new = np.empty(len(wgrid))
+
+        # Calulate the derivative of the old value function
+
+        vprime_derivative = pchip(wgrid, vprime_old)
+
+        # Use the threshold function to find the threshold w_star 
+
+        w_star = opt.fsolve(threshold, 0.5, args=(vprime_derivative, phi, R, beta))
+
+        # Solve for the optimal Vprime, kprime, bprime for w < w_star
+
+        for i in range(len(wgrid)):
+
+            if wgrid[i] < w_star:
+
+                # Solve for the optimal kprime
+
+                kprime[i] = opt.fsolve(foc_constrained, 0.5, args=(wgrid[i], vprime_derivative, phi, R, beta))
+
+                # Solve for the optimal bprime
+
+                bprime[i] = 0
+
+            else:
+
+                # Solve for the optimal bprime
+
+                bprime[i] = opt.fsolve(foc_unconstrained, 0.5, args=(wgrid[i], vprime_derivative, phi, R, beta))
+
+                # Solve for the optimal kprime
+
+                kprime[i] = kstar_fn(R)
+
+            # Calculate the new value function
+
+            vprime_new[i] = utility_fn(wgrid[i] - bprime[i] - kprime[i]) + beta * vprime_old(wgrid[i] - bprime[i] - kprime[i])
+
+        # Calculate the distance between the old and new value functions
+
+        dist = np.max(np.abs(vprime_new - vprime_old))
+
+        # Update the old value function with the new value function. 
+
+        vprime_old = vprime_new
+
+    return kprime, bprime, vprime_new
+
+
 
 
 
