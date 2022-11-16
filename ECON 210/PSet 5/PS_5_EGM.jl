3eba2466d03c786d6029688e21f5bd0cdd84da6b
Tom Rutter <67977402+TomRutter42@users.noreply.github.com>
2022-11-15 19:26:02 -0800

    Update PS_5_EGM.jl
    tingkang's edits

--- ECON 210/PSet 5/PS_5_EGM.jl
+++ ECON 210/PSet 5/PS_5_EGM.jl
@@ -1,5 +1,5 @@
-# Solve the 1991 Deaton consumption-savings problem 
-# using the endogenous grid method. 
+# Solve the 1991 Deaton consumption-savings problem
+# using the endogenous grid method.
 
 # set current folder as the working directory
 cd(@__DIR__)
@@ -11,7 +11,7 @@ println(" ")
 println("Start of Run")
 println(" ")
 
-# Packages 
+# Packages
 using Distributions
 using Plots
 using QuantEcon
@@ -23,7 +23,7 @@ using QuantEcon
 ## Utility function
 
 function u(c, γ)
-    if c <= 0.01 
+    if c <= 0.01
         return -100000
     elseif γ == 1
         return log(c)
@@ -35,7 +35,7 @@ end
 ## Derivative of utility function
 
 function u′(c, γ)
-    if c <= 0.01 
+    if c <= 0.01
         return 1000
     elseif γ == 1
         return 1/c
@@ -52,22 +52,22 @@ end
 
 # ============================================================================= #
 
-# Parameters 
-r = 0.05 
-δ = 0.10 
-ρ = 2 
+# Parameters
+r = 0.05
+δ = 0.10
+ρ = 2
 
 # ============================================================================= #
 
-# EGM 
+# EGM
 
 # ----------------------------------------------------------------------------- #
 
-## Step 1: Define the transition probabilities for the income process, 
-### given the persistence parameter of the AR(1) ψ, mean μ, and 
+## Step 1: Define the transition probabilities for the income process,
+### given the persistence parameter of the AR(1) ψ, mean μ, and
 ### standard deviation σ, using the Tauchen method.
 
-### Construct transition matrix for the AR(1) process. 
+### Construct transition matrix for the AR(1) process.
 ny = 100 # number of grid points for income y
 ψ = 0 # persistence parameter of the AR(1) process
 σ = 10.0 # standard deviation of the income process
@@ -84,10 +84,10 @@ display(y_grid)
 
 # ----------------------------------------------------------------------------- #
 
-## Step 2: Construct a grid on (b′, y) 
+## Step 2: Construct a grid on (b′, y)
 
-### Define the grid sizes. 
-nbp = 500 # number of grid points for bond choice b′ 
+### Define the grid sizes.
+nbp = 500 # number of grid points for bond choice b′
 
 ### Define the grids.
 ### Use a log-spaced grid for saving b′.
@@ -99,13 +99,13 @@ display(b′_grid)
 
 # ----------------------------------------------------------------------------- #
 
-## Step 3: Form a guess for the consumption function 
+## Step 3: Form a guess for the consumption function
 
 ### Define the guess for the consumption function.
 c_old = zeros(nbp, ny)
-### Fill in c_old with r * b′ + y. 
-for i in 1:nbp 
-    for j in 1:ny 
+### Fill in c_old with r * b′ + y.
+for i in 1:nbp
+    for j in 1:ny
         c_old[i, j] = r * b′_grid[i] + y_grid[j]
     end
 end
@@ -116,9 +116,9 @@ display(c_old)
 
 # ----------------------------------------------------------------------------- #
 
-## Step 4: Calculate RHS of Euler equation. 
+## Step 4: Calculate RHS of Euler equation.
 
-### Create a function to calculate the RHS of the Euler equation. 
+### Create a function to calculate the RHS of the Euler equation.
 
 function calculate_RHS(r, δ, ρ, Π, b′_grid, y_grid, c_old)
 
@@ -154,8 +154,8 @@ display(calculate_RHS(r, δ, ρ, Π, b′_grid, y_grid, c_old))
 
 ### Create a function to solve for the consumption function for b′.
 
-### Note this is the heart of the speedup---here we simply solve for c 
-### analytically rather than solving for a root numerically. 
+### Note this is the heart of the speedup---here we simply solve for c
+### analytically rather than solving for a root numerically.
 
 function solve_c(r, δ, ρ, Π, b′_grid, y_grid, c_old)
 
@@ -186,11 +186,11 @@ display(solve_c(r, δ, ρ, Π, b′_grid, y_grid, c_old))
 
 # ----------------------------------------------------------------------------- #
 
-## Step 6: Use the budget constraint to solve for savings function 
-## b(b′, y), where b(b′, y) is the value of assets today that would 
-## lead the consumer to have b′ tomorrow, if income is y today. 
+## Step 6: Use the budget constraint to solve for savings function
+## b(b′, y), where b(b′, y) is the value of assets today that would
+## lead the consumer to have b′ tomorrow, if income is y today.
 
-### Here we are calculating the endogenous grid. 
+### Here we are calculating the endogenous grid.
 
 function calculate_savings_consumption(r, δ, ρ, Π, b′_grid, y_grid, c_old)
 
@@ -209,7 +209,7 @@ function calculate_savings_consumption(r, δ, ρ, Π, b′_grid, y_grid, c_old)
     for (i, b′) in enumerate(b′_grid)
         for (j, y) in enumerate(y_grid)
             b_endog_grid[i, j] = (1 / (1 + r)) * (c_endog_grid[i, j] + b′_grid[i] - y_grid[j])
-            ## threshold that gets the borrowing constraint to bind next period. 
+            ## threshold that gets the borrowing constraint to bind next period.
             b_thr[j] = (1 / (1 + r)) * (c_endog_grid[1, j] - y_grid[j])
         end
     end
@@ -247,13 +247,13 @@ function update_c(r, δ, ρ, Π, b′_grid, y_grid, c_old)
     # # Create an empty matrix to store the updated consumption function
     c_new = zeros(nbp, ny)
 
-    ## We have a matrix of current assets b_opt as a function of current 
-    ## income y and next period assets b′. 
-    ## We want to update the consumption function, which gives a level of 
-    ## consumption corresponding to bonds tomorrow b′ and current income y. 
-    ## We can do this by interpolating the consumption function c_opt. 
+    ## We have a matrix of current assets b_opt as a function of current
+    ## income y and next period assets b′.
+    ## We want to update the consumption function, which gives a level of
+    ## consumption corresponding to bonds tomorrow b′ and current income y.
+    ## We can do this by interpolating the consumption function c_opt.
     ## For each b′ and y, we want to linearly interpolate the consumption
-    ## function c_opt between the two grid points in b_opt that bracket b′. 
+    ## function c_opt between the two grid points in b_opt that bracket b′.
 
     # Loop over the grid points for b′ and y
 
@@ -261,31 +261,31 @@ function update_c(r, δ, ρ, Π, b′_grid, y_grid, c_old)
         for (j, y) in enumerate(y_grid)
 
             # Case 1: Liquidity Constraint is binding.
-            
+
             if b′ <= b_thr[j]
                 c_new[i, j] = y + (1 + r) * b′ + 0
-            else 
+            else
                 # Case 2: Liquidity Constraint is not binding.
-                # Just use the Euler equation. 
-                ## Find the index of the first grid point in b_opt that is greater 
-                ## than b′. 
+                # Just use the Euler equation.
+                ## Find the index of the first grid point in b_opt that is greater
+                ## than b′.
                 ind = findfirst(b_endog_grid[:, j] .> b′)
 
-                ## If b′ is greater than the largest grid point in b_opt, then 
-                ## set the index to be the last grid point in b_opt. 
+                ## If b′ is greater than the largest grid point in b_opt, then
+                ## set the index to be the last grid point in b_opt.
                 if ind == nothing
                     ind = nbp
                 end
 
-                ## If b′ is less than the smallest grid point in b_opt, then 
-                ## set the index to be the first grid point in b_opt. 
+                ## If b′ is less than the smallest grid point in b_opt, then
+                ## set the index to be the first grid point in b_opt.
                 if ind == 1
                     ind = 2
                 end
 
-                ## Linearly interpolate the consumption function c_opt between 
-                ## the two grid points in b_opt that bracket b′. 
-                c_new[i, j] = c_opt[ind - 1, j] + (c_opt[ind, j] - c_opt[ind - 1, j]) * 
+                ## Linearly interpolate the consumption function c_opt between
+                ## the two grid points in b_opt that bracket b′.
+                c_new[i, j] = c_opt[ind - 1, j] + (c_opt[ind, j] - c_opt[ind - 1, j]) *
                     (b′ - b_endog_grid[ind - 1, j]) / (b_endog_grid[ind, j] - b_endog_grid[ind - 1, j])
             end
         end
@@ -301,7 +301,7 @@ display(update_c(r, δ, ρ, Π, b′_grid, y_grid, c_old))
 
 # ----------------------------------------------------------------------------- #
 
-## Step 7: Iterate the above until convergence. 
+## Step 7: Iterate the above until convergence.
 
 ### Create a function to iterate the above until convergence.
 
@@ -314,11 +314,11 @@ function iterate(r, δ, ρ, Π, b′_grid, y_grid, c_old, tol)
     # Create an empty matrix to store the updated consumption function
     c_new = zeros(nbp, ny)
 
-    # Create an empty matrix to store the distance between the old and new 
+    # Create an empty matrix to store the distance between the old and new
     # consumption functions
     dist = zeros(nbp, ny)
 
-    # Create an empty matrix to store the maximum distance between the old 
+    # Create an empty matrix to store the maximum distance between the old
     # and new consumption functions
     max_dist = 1
 
@@ -334,7 +334,7 @@ function iterate(r, δ, ρ, Π, b′_grid, y_grid, c_old, tol)
         # Calculate the distance between the old and new consumption functions
         dist = abs.(c_new - c_old)
 
-        # Calculate the maximum distance between the old and new consumption 
+        # Calculate the maximum distance between the old and new consumption
         # functions
         max_dist = maximum(dist)
 
@@ -348,12 +348,12 @@ function iterate(r, δ, ρ, Π, b′_grid, y_grid, c_old, tol)
         # Update the old consumption function
         c_old = deepcopy(c_new)
 
-        
+
 
         # Update the iteration number
         iter += 1
 
-        
+
 
     end
 
@@ -361,7 +361,7 @@ function iterate(r, δ, ρ, Π, b′_grid, y_grid, c_old, tol)
 
 end
 
-## Apply function 
+## Apply function
 
 consumption_fn = iterate(r, δ, ρ, Π, b′_grid, y_grid, c_old, 10^(-6))
 
@@ -369,13 +369,13 @@ println(" ")
 println("The consumption function, after iterating, is: ")
 display(consumption_fn)
 
-## Plot the consumption function for each level of bonds b. 
+## Plot the consumption function for each level of bonds b.
 
-## Plot consumption_fn when income = 70 
+## Plot consumption_fn when income = 70
 
-## then cash on hand = 70 + b 
+## then cash on hand = 70 + b
 
-## doesn't matter here since income is iid. 
+## doesn't matter here since income is iid.
 
 plot(b′_grid .+ 70, consumption_fn[:, 1])
 ## add line y = x
@@ -383,18 +383,18 @@ plot(b′_grid .+ 70, consumption_fn[:, 1])
 
 # ----------------------------------------------------------------------------- #
 
-# Solve the Consumption savings case problem for the following four cases: 
-## 1) ρ = 2, σ = 10 
-## 2) ρ = 2, σ = 15 
+# Solve the Consumption savings case problem for the following four cases:
+## 1) ρ = 2, σ = 10
+## 2) ρ = 2, σ = 15
 ## 3) ρ = 3, σ = 10
 ## 4) ρ = 3, σ = 15
 
 consumption_fn1 = iterate(r, δ, 2.0, Π, b′_grid, y_grid, c_old, 10^(-6))
 consumption_fn3 = iterate(r, δ, 3.0, Π, b′_grid, y_grid, c_old, 10^(-6))
 
-## Small note: I tactically chose the standard deviations to calculate out 
-## to in the discretization so that both y's start from 55, i.e., 
-## first column of consumption function corresponds to 55 income + bonds. 
+## Small note: I tactically chose the standard deviations to calculate out
+## to in the discretization so that both y's start from 55, i.e.,
+## first column of consumption function corresponds to 55 income + bonds.
 
 σ = 15.0 # standard deviation of the income process
 MChain = QuantEcon.tauchen(ny, ψ, σ, 0, 3.0)
@@ -411,7 +411,7 @@ plot!(b′_grid .+ 55, consumption_fn2[:, 1], label = "ρ = 2, σ = 15")
 plot!(b′_grid .+ 55, consumption_fn3[:, 1], label = "ρ = 3, σ = 10")
 plot!(b′_grid .+ 55, consumption_fn4[:, 1], label = "ρ = 3, σ = 15")
 
-# ## Add a dotted 45 degree line 
+# ## Add a dotted 45 degree line
 # plot!(b′_grid .+ 55, b′_grid .+ 55, label = "45 Degree Line", ls = :dot)
 
 # Label the y axis as "Consumption"
@@ -419,22 +419,22 @@ ylabel!("Consumption")
 # Label the x axis as "Cash on Hand"
 xlabel!("Cash on Hand")
 
-# Move the legend to the top left corner 
+# Move the legend to the top left corner
 plot!(legend = :topleft)
 
-# Restrict the axis to be 70 to 320 
+# Restrict the axis to be 70 to 320
 
 plot!(xlim = (55, 320), ylim = (50, 150))
 
-# Save the graph 
+# Save the graph
 savefig("ConsumptionCases.png")
 
 # ----------------------------------------------------------------------------- #
 
 # ----------------------------------------------------------------------------- #
 
-# Simulate an income process for 200 periods where μ = 100 
-# and σ = 10. 
+# Simulate an income process for 200 periods where μ = 100
+# and σ = 10.
 
 σ = 10.0 # standard deviation of the income process
 MChain = QuantEcon.tauchen(ny, ψ, σ, 0, 4.5)
@@ -447,7 +447,7 @@ R = 1.05
 
 y_t = rand(Normal(100, 10), 200)
 
-## Using the optimal saving function s_opt1, plot how wealth s + y 
+## Using the optimal saving function s_opt1, plot how wealth s + y
 ## evolves over time.
 
 # Define an optimal saving function based on the consumption function
@@ -469,50 +469,50 @@ c_t = ones(200)
 #         c_t[i] = consumption_fn1[1, y_ind]
 #         s_t[i] = y_t[i] - c_t[i]
 #     else
-         
+
 #         c_t[i] = consumption_fn1[findmin(abs.(b′_grid .- R * s_t[i - 1]))[2], y_ind]
 #         s_t[i] = R * s_t[i - 1] + y_t[i] - c_t[i]
 
 #     end
 # end
 
-for i in 1:200 
+for i in 1:200
     # find index of y_grid closest to y_t[i]
     y_ind = findmin(abs.(y_grid .- y_t[i]))[2]
     if i == 1
         s_t[i] = s_optimal_1[1, y_ind]
-        # ## get rid of rounding error that can cause s_t to fall slightly below 0 
+        # ## get rid of rounding error that can cause s_t to fall slightly below 0
         # if s_t[i] < 0
         #     s_t[i] = 0
         # end
         c_t[i] = y_t[i] - s_t[i]
     else
         s_t[i] = s_optimal_1[findmin(abs.(b′_grid .- R * s_t[i - 1]))[2], y_ind]
-        ## get rid of rounding error that can cause s_t to fall slightly below 0 
+        ## get rid of rounding error that can cause s_t to fall slightly below 0
         if s_t[i] < 0
             s_t[i] = 0
         end
         c_t[i] = y_t[i] + s_t[i - 1] - s_t[i]
     end
-end 
+end
 
-## Plot the paths of income, saving, and consumption 
+## Plot the paths of income, saving, and consumption
 
-plot(y_t, label = "Income", xlabel = "Time", ylabel = "Income", legend = :topleft)
+plot(y_t, label = "Income", xlabel = "Time", ylabel = "Savings     Consumption  Income    ", legend = :outertop)
 plot!(s_t, label = "Savings")
 c_t_down = c_t .- 40
 plot!(c_t_down, label = "Consumption - 40")
 
-## Save the plot: 
+## Save the plot:
 savefig("IncomeSavingConsumptionEG.png")
 
 # ----------------------------------------------------------------------------- #
 
 # AR(1) process
 
-### Construct transition matrix for the AR(1) process. 
+### Construct transition matrix for the AR(1) process.
 # ny = 100 # number of grid points for income y
-ψ = 0.70 # persistence parameter of the AR(1) process, this is different to before. 
+ψ = 0.70 # persistence parameter of the AR(1) process, this is different to before.
 σ = 10.0 # standard deviation of the income process
 μ = 100.0 # mean of the income process
 MChain = QuantEcon.tauchen(ny, ψ, σ, 0, 3.0)
@@ -527,19 +527,24 @@ display(y_grid)
 
 consumption_fn_ar1 = iterate(0.02, 0.05, 2.0, Π, b′_grid, y_grid, c_old, 10^(-6))
 
-## Plot the consumption function for the AR(1) process, 
+## Plot the consumption function for the AR(1) process,
 ## for given cash on hand, by income level.
 
 plot(b′_grid .+ y_grid[1], consumption_fn_ar1[:, 1])
-for i in 2:50 
-    ## move to the next loop if i is not multiple of 10 
+for i in 2:50
+    ## move to the next loop if i is not multiple of 10
     if i % 10 != 0
         continue
     end
     plot!(b′_grid .+ y_grid[i], consumption_fn_ar1[:, i])
 end
 
-## no legend on the chart 
+# Label the y axis as "Consumption"
+ylabel!("Consumption")
+# Label the x axis as "Cash on Hand"
+xlabel!("Cash on Hand")
+
+## no legend on the chart
 plot!(legend = false)
 
 savefig("AR1_by_.png")
@@ -562,35 +567,30 @@ for (i, b) in enumerate(b′_grid)
     end
 end
 
-for i in 1:200 
+for i in 1:200
     # find index of y_grid closest to y_t[i]
     y_ind = findmin(abs.(y_grid .- y_t[i]))[2]
     if i == 1
         s_t[i] = s_optimal_1[1, y_ind]
-        # ## get rid of rounding error that can cause s_t to fall slightly below 0 
+        # ## get rid of rounding error that can cause s_t to fall slightly below 0
         # if s_t[i] < 0
         #     s_t[i] = 0
         # end
         c_t[i] = y_t[i] - s_t[i]
     else
         s_t[i] = s_optimal_1[findmin(abs.(b′_grid .- R * s_t[i - 1]))[2], y_ind]
-        ## get rid of rounding error that can cause s_t to fall slightly below 0 
+        ## get rid of rounding error that can cause s_t to fall slightly below 0
         if s_t[i] < 0
             s_t[i] = 0
         end
         c_t[i] = y_t[i] + s_t[i - 1] - s_t[i]
     end
-end 
+end
 
-plot(y_t, label = "Income", xlabel = "Time", ylabel = "Income", legend = :topleft)
+plot(y_t, label = "Income", xlabel = "Time", ylabel = "Savings     Consumption  Income    ", legend = :outertop)
 plot!(s_t, label = "Savings")
 c_t_down = c_t .- 40
 plot!(c_t_down, label = "Consumption - 40")
 
-## Save the plot: 
+## Save the plot:
 savefig("IncomeSavingConsumptionEG_AR.png")
-
-
-
-
-
